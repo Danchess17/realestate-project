2f7b1532cb7e28a00c1850fe02063beb8f066d35
ishhanjan daniel <ishkhanian.da@phystech.edu>
2023-04-16 10:09:08 +0000

    Update realestate_ddl.sql

--- realestate_ddl.sql
+++ realestate_ddl.sql
@@ -2,27 +2,31 @@ create schema real_estate;
 
 create table real_estate.account (
     account_id int not null, 
-    online boolean not null, 
-    telephone varchar(255) not null,
+    online_flg boolean not null, 
+    telephone_no varchar(255) not null,
     email varchar(255) not null,
-    account_type varchar(255) not null,
-    registration_time timestamp without time zone not null,
-    complaint_counter int not null,
-    transaction_counter int not null,
+    realtor_flg boolean not null,
+    registration_dttm timestamp without time zone not null,
+    complaint_cnt int not null,
+    transaction_cnt int not null,
+    first_name varchar(255) not null,
+    second_name varchar(255) not null, 
 
     constraint account_pk primary key (account_id)
 );
 
 create table real_estate.account_version_history (
     change_id int not null,
-    change_time timestamp without time zone not null,
+    change_dttm timestamp without time zone not null,
     account_id int not null, 
-    new_online boolean not null, 
-    new_telephone varchar(255) not null,
+    new_online_flg boolean not null, 
+    new_telephone_no varchar(255) not null,
     new_email varchar(255) not null,
-    new_account_type varchar(255) not null,
-    new_complaint_counter int not null,
-    new_transaction_counter int not null,
+    new_realtor_flg boolean not null,
+    new_complaint_cnt int not null,
+    new_transaction_cnt int not null,
+    new_first_name varchar(255) not null,
+    new_second_name varchar(255) not null, 
 
     constraint change_account_pk primary key (change_id), 
 
@@ -33,32 +37,28 @@ create table real_estate.account_version_history (
 
 create table real_estate.realtor (
     realtor_id int not null, 
-    first_name varchar(255) not null,
-    second_name varchar(255) not null, 
-    from_company boolean not null, 
-    checked_realtor boolean not null, 
-    request_got_amount int not null, 
-    rating decimal,
+    from_company_flg boolean not null, 
+    checked_realtor_flg boolean not null, 
+    request_got_cnt int not null, 
+    realtor_rate decimal,
 
-    constraint rating_range_check
-    check(rating is null or rating between 0 and 10),
+    constraint realtor_rate_range_check
+    check(realtor_rate is null or realtor_rate between 0 and 10),
 
     constraint realtor_pk primary key (realtor_id)
 );
 
 create table real_estate.realtor_history (
     change_id int not null, 
-    change_time timestamp without time zone not null,
+    change_dttm timestamp without time zone not null,
     realtor_id int not null, 
-    new_first_name varchar(255) not null,
-    new_second_name varchar(255) not null, 
-    new_from_company boolean not null, 
-    new_checked_realtor boolean not null, 
-    new_request_got_amount int not null, 
-    new_rating decimal,
+    new_from_company_flg boolean not null, 
+    new_checked_realtor_flg boolean not null, 
+    new_request_got_cnt int not null, 
+    new_realtor_rate decimal,
 
-    constraint new_rating_range_check
-    check(new_rating is null or new_rating between 0 and 10),
+    constraint new_realtor_rate_range_check
+    check(new_realtor_rate is null or new_realtor_rate between 0 and 10),
 
     constraint change_realtor_pk primary key (change_id), 
 
@@ -69,20 +69,16 @@ create table real_estate.realtor_history (
 
 create table real_estate.client (
     client_id int not null, 
-    first_name varchar(255) not null, 
-    second_name varchar(255) not null, 
-    request_sent_amount int not null,
+    request_sent_cnt int not null,
 
     constraint client_pk primary key (client_id)
 );
 
 create table real_estate.client_history (
     change_id int not null, 
-    change_time timestamp without time zone not null,
+    change_dttm timestamp without time zone not null,
     client_id int not null, 
-    new_first_name varchar(255) not null, 
-    new_second_name varchar(255) not null, 
-    new_request_sent_amount int not null,
+    new_request_sent_cnt int not null,
 
     constraint change_client_pk primary key (change_id), 
 
@@ -94,24 +90,24 @@ create table real_estate.client_history (
 create table real_estate.apartment (
     apartment_id int not null,
     apartment_type varchar(255) not null, 
-    total_area int not null, 
-    living_area int, 
-    kitchen_area int, 
+    total_area_amt int not null, 
+    living_area_amt int, 
+    kitchen_area_amt int, 
     apartment_address varchar(255) not null,
-    flat_counter int not null, 
-    balcony_counter int not null,
+    flat_cnt int not null, 
+    balcony_cnt int not null,
 
-    constraint appartment_pk primary key (apartment_id)
+    constraint apartment_pk primary key (apartment_id)
 );
 
 create table real_estate.announcement (
     announcement_id int not null, 
     apartment_id int not null, 
-    apartment_description varchar(1000),
+    apartment_desc varchar(1000),
     announcement_type varchar(255) not null, 
-    announcement_price varchar(255) not null, 
-    made_by_realtor boolean not null,
-    active boolean not null,
+    announcement_price_cnt varchar(255) not null, 
+    made_by_realtor_flg boolean not null,
+    active_flg boolean not null,
 
     constraint announcement_pk primary key (announcement_id),
 
@@ -122,14 +118,14 @@ create table real_estate.announcement (
 
 create table real_estate.transaction_history (
     transaction_id int not null,
-    transaction_time timestamp without time zone not null,
+    transaction_dttm timestamp without time zone not null,
     apartment_id int not null, 
     seller_id int not null, 
     seller_realtor_id int, 
     buyer_id int not null, 
     buyer_realtor_id int, 
     transaction_type varchar(255) not null, 
-    transaction_price int not null,
+    transaction_price_cnt int not null,
 
     constraint transaction_pk primary key (transaction_id),
 
